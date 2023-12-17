913b6b438aaeb7305cf0396fa38d861966d56e31
I_fors_I _ <burlessons@gmail.com>
2023-12-17 13:41:44 +0500

    slider for main page

---
+++ WorkProject/sliderMainPage.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.getElementById('slider');
+    const slides = slider.querySelector('.slides');
+    let slideIndex = 0;
+    let interval;
+
+    function moveSlide() {
+        slideIndex = (slideIndex + 1) % slides.children.length;
+        updateSlidePosition();
+    }
+
+    function updateSlidePosition() {
+        slides.style.transform = `translateX(-${slideIndex * (100 / slides.children.length)}%)`;
+    }
+    
+
+    function startAutoSlide() {
+        interval = setInterval(moveSlide, 3000); // Изменяет слайд каждые 3 секунды
+    }
+
+    startAutoSlide();
+});
